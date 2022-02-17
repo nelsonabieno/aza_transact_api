026c27cce3e44acdc7031bd1cc63120ffcf461d7
Rotimi Nelson <nelsonabieno@gmail.com>
2022-02-17 13:36:38 +0100

    add model validations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,7 @@
 class Transaction < ApplicationRecord
+  validates :customer_id, numericality: { only_integer: true }
+  validates :input_amount, numericality: true
+  validates :input_amount_currency, presence: true
+  validates :output_amount_currency, presence: true
+  validates :exchange_rate, numericality: true
 end
