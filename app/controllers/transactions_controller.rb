f1269fab75b24816bfaddf6adcddbd7947c3b8c4
Rotimi Nelson <nelsonabieno@gmail.com>
2022-02-21 13:52:04 +0100

    update routes and add update action to transaction

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,4 +1,13 @@
+# Todo list
+# create a CalculateAmountService to handle exchange_rate calculations
+# check for code duplication to make it DRY
+# write unit tests for each functionality
+# update read me api documentation
+# add security authentication
+
 class TransactionsController < ApplicationController
+  before_action :find_transaction, only: [:show ,:update ]
+
   def index
     @transactions = Transaction.all
     render json: @transactions, status: :ok
@@ -20,13 +29,32 @@ class TransactionsController < ApplicationController
   end
 
   def show
-    transaction = Transaction.find(params[:id])
-    render json: transaction, status: :ok
+    render json: @transaction, status: :ok
+  end
+
+  def update
+
+    if @transaction.update(transaction_params)
+      render json: @transaction, status: :ok
+    else
+      render json: { error: @transaction.errors.full_messages}, status: :unprocessable_entity
+    end
   end
 
   private
 
   def transaction_params
     params.permit(:customer_id, :input_amount, :input_amount_currency, :output_amount_currency, :exchange_rate)
+  end
+
+  def find_transaction
+
+    transaction = Transaction.find_by_id(params[:id])
+
+    if transaction.nil?
+      render json: {message: "transaction not found"}, status: :not_found
+    else
+      @transaction = Transaction.find(params[:id])
+    end
   end
 end
